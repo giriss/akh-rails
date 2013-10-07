19b987c63b8a21ca8f150407fe9f7fe1acb75a8b
akhi <akhi@vm-0.akhi.koding.kd.io>
2013-10-07 21:21:59 +0000

    fool n final

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App2::Application.config.secret_key_base = '660d80ac5ce5d54c83e5fe0bac0e015f8a8ab6edae7ef2bbe0e29d0214d0a4fc354d462a95f85a868287ef66cc3a59a0d543a459668e1a7fcbee2cf1d7570687'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+App2::Application.config.session_store :cookie_store, key: '_app2_session'
