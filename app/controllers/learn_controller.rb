cb1db31b626d670c4a8f46c7c00fef8dc329c596
Girish Gopaul <akhile@dr.com>
2013-10-19 00:09:08 -0700

    Update learn_controller.rb

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -193,24 +193,26 @@ class LearnController < ApplicationController
     @post.set_form_data @data
     @req = @https.start {|https| https.request @post}
 =end
-@api = PayPal::SDK::Merchant::API.new
-@mass_pay = @api.build_mass_pay({
-  :ReceiverType => "EmailAddress",
-  :MassPayItem => [{
-    :ReceiverEmail => "akhile@dr.com",
-    :Amount => {
-      :currencyID => "USD",
-      :value => "10.00" } }] })
-
-# Make API call & get response
-@mass_pay_response = @api.mass_pay(@mass_pay)
-
-# Access Response
-if @mass_pay_response.success?
-  @ret = @mass_pay_response.Ack
-else
-  @ret = @mass_pay_response.Errors
-end
+    @api = PayPal::SDK::Merchant::API.new
+    @mass_pay = @api.build_mass_pay({
+      :ReceiverType => "EmailAddress",
+      :MassPayItem => [{
+        :ReceiverEmail => "akhile@dr.com",
+        :Amount => {
+          :currencyID => "USD",
+          :value => "10.00" }
+          }]
+        })
+    
+    # Make API call & get response
+    @mass_pay_response = @api.mass_pay(@mass_pay)
+    
+    # Access Response
+    if @mass_pay_response.success?
+      @ret = @mass_pay_response.Ack
+    else
+      @ret = @mass_pay_response.Errors
+    end
     @ret = "Post to send money using PayPal! yeah I did it ;) !! ^_^<br />" + @ret
     render text: @ret
   end
@@ -227,81 +229,81 @@ end
   :password  => "1381743824",
   :signature => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr" )
 =end
-@api = PayPal::SDK::Merchant::API.new
-
-# Build request object
-@set_express_checkout = @api.build_set_express_checkout({
-  :SetExpressCheckoutRequestDetails => {
-    :ReturnURL => "http://gagkas.tk/learn/lesson10",
-    :CancelURL => "http://gagkas.tk/learn/lesson8",
-    :PaymentDetails => [{
-      :OrderTotal => {
-        :currencyID => "USD",
-        :value => "12.0" },
-      :ItemTotal => {
-        :currencyID => "USD",
-        :value => "12" },
-      :PaymentDetailsItem => [{
-        :Name => "Deposit money",
-        :Quantity => 1,
-        :Amount => {
-          :currencyID => "USD",
-          :value => "12" },
-        :ItemCategory => "Digital" }],
-      :PaymentAction => "Sale" }] } })
-
-# Make API call & get response
-@set_express_checkout_response = @api.set_express_checkout(@set_express_checkout)
-
-# Access Response
-if @set_express_checkout_response.success?
-  @token = @set_express_checkout_response.Token
-else
-  @errors = @set_express_checkout_response.Errors
-end
+    @api = PayPal::SDK::Merchant::API.new
+    
+    # Build request object
+    @set_express_checkout = @api.build_set_express_checkout({
+      :SetExpressCheckoutRequestDetails => {
+        :ReturnURL => "http://gagkas.tk/learn/lesson10",
+        :CancelURL => "http://gagkas.tk/learn/lesson8",
+        :PaymentDetails => [{
+          :OrderTotal => {
+            :currencyID => "USD",
+            :value => "12.0" },
+          :ItemTotal => {
+            :currencyID => "USD",
+            :value => "12" },
+          :PaymentDetailsItem => [{
+            :Name => "Deposit money",
+            :Quantity => 1,
+            :Amount => {
+              :currencyID => "USD",
+              :value => "12" },
+            :ItemCategory => "Digital" }],
+          :PaymentAction => "Sale" }] } })
+    
+    # Make API call & get response
+    @set_express_checkout_response = @api.set_express_checkout(@set_express_checkout)
+    
+    # Access Response
+    if @set_express_checkout_response.success?
+      @token = @set_express_checkout_response.Token
+    else
+      @errors = @set_express_checkout_response.Errors
+    end
 =begin
-    @data = {
-      :METHOD => "setExpressCheckout",
-      :VERSION => "90",
-      :USER => "akhil05_api1.mail.com",
-      :PWD => "1381743824",
-      :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
-      :PAYMENTREQUEST_0_AMT => "10",
-      :PAYMENTREQUEST_0_CURRENCYCODE => "USD",
-      :PAYMENTREQUEST_0_PAYMENTACTION => "SALE",
-      :returnUrl => "http://gagkas.tk/learn/lesson10",
-      :cancelUrl => "http://gagkas.tk/learn/lesson8"
-    }
-    @url = "https://api-3t.sandbox.paypal.com/nvp"
-    @uri = URI @url
-    @uri = URI.parse @url
-    @https = Net::HTTP.new @uri.host, @uri.port
-    @https.use_ssl = true
-    @post = Net::HTTP::Post.new @uri.path
-    @post.set_form_data @data
-    @req = @https.start {|https| https.request @post}
-    @token = @req.body.split('TOKEN=')[1].split('&')[0]
+        @data = {
+          :METHOD => "setExpressCheckout",
+          :VERSION => "90",
+          :USER => "akhil05_api1.mail.com",
+          :PWD => "1381743824",
+          :SIGNATURE => "AP8wAEeWcdquPOE6hUJmW1U9KBctAiUTu.2IbHJTknQnojFEGJvXtVHr",
+          :PAYMENTREQUEST_0_AMT => "10",
+          :PAYMENTREQUEST_0_CURRENCYCODE => "USD",
+          :PAYMENTREQUEST_0_PAYMENTACTION => "SALE",
+          :returnUrl => "http://gagkas.tk/learn/lesson10",
+          :cancelUrl => "http://gagkas.tk/learn/lesson8"
+        }
+        @url = "https://api-3t.sandbox.paypal.com/nvp"
+        @uri = URI @url
+        @uri = URI.parse @url
+        @https = Net::HTTP.new @uri.host, @uri.port
+        @https.use_ssl = true
+        @post = Net::HTTP::Post.new @uri.path
+        @post.set_form_data @data
+        @req = @https.start {|https| https.request @post}
+        @token = @req.body.split('TOKEN=')[1].split('&')[0]
 =end
     redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=#{@token}"
   end
   
   def lesson10
-@api = PayPal::SDK::Merchant::API.new
-
-# Build request object
-@token = params[:token]
-@get_express_checkout_details = @api.build_get_express_checkout_details({
-  :Token => @token })
-
-# Make API call & get response
-@get_express_checkout_details_response = @api.get_express_checkout_details(@get_express_checkout_details)
-
-# Access Response
-if @get_express_checkout_details_response.success?
-  @get_express_checkout_details_response.GetExpressCheckoutDetailsResponseDetails
-else
-  @get_express_checkout_details_response.Errors
-end
+    @api = PayPal::SDK::Merchant::API.new
+    
+    # Build request object
+    @token = params[:token]
+    @get_express_checkout_details = @api.build_get_express_checkout_details({
+      :Token => @token })
+    
+    # Make API call & get response
+    @get_express_checkout_details_response = @api.get_express_checkout_details(@get_express_checkout_details)
+    
+    # Access Response
+    if @get_express_checkout_details_response.success?
+      @get_express_checkout_details_response.GetExpressCheckoutDetailsResponseDetails
+    else
+      @get_express_checkout_details_response.Errors
+    end
 =begin
     @token = params[:token]
     @data = {
@@ -345,29 +347,28 @@ end
     @post.set_form_data @data
     @req = @https.start {|https| https.request @post}
 =end
-@do_express_checkout_payment = @api.build_do_express_checkout_payment({
-  :DoExpressCheckoutPaymentRequestDetails => {
-    :PaymentAction => "Sale",
-    :Token => @token,
-    :PayerID => @payerid,
-    :PaymentDetails => [{
-      :OrderTotal => {
-        :currencyID => "USD",
-        :value => @amt },
-    }] } })
+    @do_express_checkout_payment = @api.build_do_express_checkout_payment({
+      :DoExpressCheckoutPaymentRequestDetails => {
+        :PaymentAction => "Sale",
+        :Token => @token,
+        :PayerID => @payerid,
+        :PaymentDetails => [{
+          :OrderTotal => {
+            :currencyID => "USD",
+            :value => @amt },
+        }] } })
 
-# Make API call & get response
-@do_express_checkout_payment_response = @api.do_express_checkout_payment(@do_express_checkout_payment)
-
-# Access Response
-if @do_express_checkout_payment_response.success?
-  @do_express_checkout_payment_response.DoExpressCheckoutPaymentResponseDetails
-  @do_express_checkout_payment_response.FMFDetails
-  @ret = @do_express_checkout_payment_response.Ack
-else
-  @ret = @do_express_checkout_payment_response.Errors.join
-end
-
+    # Make API call & get response
+    @do_express_checkout_payment_response = @api.do_express_checkout_payment(@do_express_checkout_payment)
+    
+    # Access Response
+    if @do_express_checkout_payment_response.success?
+      @do_express_checkout_payment_response.DoExpressCheckoutPaymentResponseDetails
+      @do_express_checkout_payment_response.FMFDetails
+      @ret = @do_express_checkout_payment_response.Ack
+    else
+      @ret = @do_express_checkout_payment_response.Errors.join
+    end
     render text: @ret
   end
 
